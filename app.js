564aa1eccf30bbd61e504247741e01bcfa012ebf
KhaiCoding <KhaiCoding@github.com>
2019-09-24 18:04:44 +0900

    Basic Controllers and Routers functions

---
+++ app.js
@@ -0,0 +1,28 @@
+// import morgan from "morgan";
+import express from "express";
+import morgan  from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express();
+
+
+//  사이트에서 일어나는 모든 연결에 다음의 middleware를 실행
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev")); /*morgan이 기록담당이어서 맨 아래 */
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+// 누군가 내 파일을 import 했을 때, app object를 주겠다는 의미
+export default app;

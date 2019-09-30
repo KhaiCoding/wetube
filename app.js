0355a4a08713a8d4d88fb8b6278ee9c0ac5b2adf
KhaiCoding <KhaiCoding@github.com>
2019-09-30 16:48:07 +0900

    Backend 50% finished
    Still needed to fix searching function.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import 순서는 알파벳 순서
 import express from "express";
-import morgan  from "morgan";
+import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -12,16 +12,15 @@ import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
-
 //  사이트에서 일어나는 모든 연결에 다음의 middleware를 실행
+//  morgan이 기록담당이어서 맨 아래
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan("dev")); /*morgan이 기록담당이어서 맨 아래 */
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -2,80 +2,98 @@ import routes from "../routes";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-    try {
-        const videos = await Video.find({});
-        res.render("home", {pageTitle : "Home", videos });
-    } catch(error){
-        // console.log(error);
-        res.render("home", {pageTitle : "Home", videos: [] });
-    }
+  try {
+    // sort({_id :-1}) meaning : make sort Backwards
+    const videos = await Video.find({}).sort({ _id: -1 });
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    // console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
-export const search = (req, res) => {
-    const {
-        query: { term: searchingBy }
-    } = req;
-    // const searchingBy= req.query.term;
-    res.render("search", {pageTitle : "Search", searchingBy: searchingBy, videos});
+export const search = async (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regix: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  // const searchingBy= req.query.term;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const getUpload = (req, res) => 
-    res.render("upload", {pageTitle : "Upload"});
-export const postUpload = async(req, res) => {
-    const { 
-        body: {title, description },
-        file: { path }
-    } = req;
-    const newVideo = await Video.create({
-        fileUrl: path,
-        title,
-        description
-    });
-    console.log(newVideo);
-    // To Do : Upload and save video
-    res.redirect(routes.videoDetail(newVideo.id));
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  //   console.log(newVideo);
+  // To Do : Upload and save video
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = async (req, res) => {
-    // console.log(req.params);
-    const {
-        params: { id }
-    } = req;
-    //  바로 위에서 받은 id를 findById로 넘겨준다.
-    try{
-        const video = await Video.findById(id);
-        res.render("videoDetail", {pageTitle : "Video Detail", video});
-    } catch(error) {
-        console.log(error);
-        res.redirect(routes.home);
-    }
+  // console.log(req.params);
+  const {
+    params: { id }
+  } = req;
+  //  바로 위에서 받은 id를 findById로 넘겨준다.
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    console.log(error);
+    res.redirect(routes.home);
+  }
 };
-export const getEditVideo = async (req, res) => { 
-    const {
-        params: {id}
-    } = req;
-    try {
-        // 순서가 바뀌면 error 발생
-        const video = await Video.findById(id);
-        res.render("editVideo",{pageTitle : `Edit ${video.title}`, video});
-    } catch(error) {
-        res.redirect(routes.home);
-    }
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    // 순서가 바뀌면 error 발생
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 
 export const postEditVideo = async (req, res) => {
-    const {
-        params: {id},
-        body: {title, description}
-    } = req;
-    try {
-        await Video.findOneAndUpdate( {id}, {title, description})
-        res.redirect(routes.videoDetail(id));
-    } catch (error){
-        res.redirect(routes.home);
-    }
-
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 
-
-export const deleteVideo = (req, res) => 
-    res.render("deleteVideo",{pageTitle : "Delete Video"});
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    // look at mongoose documents, findOneAndRemove
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
+  // regardless of success, redirect to home
+  res.redirect(routes.home);
+};
